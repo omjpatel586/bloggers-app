28904f7592d4003df505cafa2f8c935af5f503b6
Om J Patel <119939918+omjpatel586@users.noreply.github.com>
2023-04-22 13:55:35 +0000

    blogapp

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css'
 import reportWebVitals from './reportWebVitals'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
-import 'bootstrap/dist/css/bootstrap.min.css"
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 const url = ["https://bloggers-app.vercel.app/"]
 
